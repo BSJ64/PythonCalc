f8966607a147afa0a37e0163e37a5f76239eddbd
Brenden St Juste <52753199+BSJ64@users.noreply.github.com>
2024-11-22 00:03:02 -0500

    Base calculator in Python

---
+++ README.md
@@ -0,0 +1,1 @@
+Code by Brenden St. Juste
---
+++ main.py
@@ -0,0 +1,17 @@
+# Original code by Brenden St. Juste
+
+
+def baseCalc(starter, base):
+    codeList = []
+    while starter != 0:
+        result = starter % base
+        codeList.append(result)
+        starter = round(starter / base)
+    print(*codeList)
+
+
+print("Input a whole number.")
+starter = int(input())
+print("Input a base to convert to.")
+base = int(input())
+baseCalc(starter, base)
